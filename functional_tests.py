3af0de3668618c3c39ad7db6c0ecd9aedc3c18b3
ludmila <ldobrela@.com>
2018-08-17 16:33:20 +0300

    Расширен первый ФТ в комментариях теперь использует unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    '''тест нового посетителя'''
 
-assert 'Django' in browser.title
+    def setUp(self):
+        '''установка'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        '''демонтаж'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''тест: можно начать список и получить его позже'''
+        #Эдит слышала ро крутое новое приложение со списком
+        #неотложных дел Она решает оценить его домашнюю страничку
+        self.browser.get('http://localhost:8000')
+
+        #Она видит что заголовок и шапка страницы говорит о списке неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+        #Ей сразу же предлагается ввести элемент списка
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

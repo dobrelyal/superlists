398ea49a471b7528e470d18ea3381aa5e68022f7
ludmila <ldobrela@.com>
2018-08-22 15:50:00 +0300

    ФТ теперь проверяет возможность ввести елемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,9 +22,27 @@ class NewVisitorTest(unittest.TestCase):
 
         #Она видит что заголовок и шапка страницы говорит о списке неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+
+        #Когда она нажимает enter страница обновляется и теперь она содержит "1 Купить павлиньи перья"в качестве елемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+        self.fail('Закончить тест')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')

6751cf4d5415750cb41eba69df15bcf3fd8e5da6
ludmila <ldobrela@.com>
2018-10-24 14:18:48 +0300

    Первые шаги для ФТ для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -46,8 +46,8 @@ class NewVisitorTest(LiveServerTestCase):
         #ее приглашают сделать запись
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-        inputbox.get_attribute('placeholder'),
-        'Enter a to-do item'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
          )
 
         #
@@ -110,3 +110,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Купить молоко', page_text)
 
         #Удовлетворенные они оба ложаться спать
+
+
+    def test_layout_and_styling(self):
+        '''тест макета и стилевого оформления'''
+        #Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #Она замечает что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        #Она начинает новый список и видит что поле ввода там
+        #тоже оккуратно центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )   

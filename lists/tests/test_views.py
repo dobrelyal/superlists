8f069f25ab24ee3513eac249f5e0eb96696ce13c
ludmila <ldobrela@.com>
2018-12-08 12:09:59 +0200

    Использована форма во всех представлениях, возврат к рабочему состоянию

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
@@ -34,13 +34,21 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        '''тест ошибки валидации отсылаются назад в шаблон домашней страницы'''
+    def test_for_invalid_input_renders_home_template(self):
+        '''тест на недопустимый ввод отображает домашний шаблон'''
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+    def test_validation_errors_are_show_on_home_page(self):
+        '''тест ошибки валидации выводятся на домашней странице'''
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        '''тест на недопустимый ввод форма передается в шаблон'''
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
 
     def test_invalid_list_items_arent_saved(self):
         '''тест сохраняются недопустимые элементы списка'''
@@ -63,6 +71,13 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
+
+    def test_displays_item_form(self):
+        '''тест отображения формы для элемента'''
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
 
     def  test_displays_only_items_for_that_list(self):
         '''тест отображаются элементы только для этого списка'''
@@ -105,14 +120,31 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
-    def test_validation_errors_and_up_on_lists_page(self):
-        '''тест ошибки валидации оканчиваются на странице списков'''
+    def post_invalid_input(self):
+        '''тест отправляет недопустимый ввод'''
         list_ = List.objects.create()
-        response = self.client.post(
+        return self.client.post(
             f'/lists/{list_.id}/',
             data={'text': ''}
         )
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        '''тест на недопустимый ввод ничего несохраняется в БД'''
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        '''тест на недопустимый ввод отображается шаблон списка'''
+        response = self.post_invalid_input()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        '''тест на недопустимый ввод форма передается в шаблон'''
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        '''тест на недопустимый ввод на странице показывается ошибка'''
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))

e85f524ab7d9b4bca51420defd657f1e532fabb6
ludmila <ldobrela@.com>
2019-01-19 14:33:28 +0200

    account1

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,8 @@ import uuid
 from django.db import models
 
 class User(models.Model):
-    '''пользователь'''
+
     email = models.EmailField(primary_key=True)
-
-
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
@@ -14,6 +12,5 @@ class User(models.Model):
 
 
 class Token(models.Model):
-    '''маркер'''
     email = models.EmailField()
     uid = models.CharField(default=uuid.uuid4, max_length=40)
--- lists/urls.py
+++ lists/urls.py
@@ -19,7 +19,7 @@ from lists import views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    #url(r'^$', views.home_page, name='home'),
+    url(r'^$', views.home_page, name='home'),
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
     #url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
